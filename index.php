d82e4987155a6a92bb3f483fee91fc7c7980f35d
Patrick Cook <pcookcollege@gmail.com>
2019-03-30 18:06:02 -0700

    Updated FontAwesome link

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	<link href="css/style.css" rel="stylesheet">
 
 	<!-- Custom Fonts -->
-	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 </head>
